554d9400795acb76a6a31b41de88ee066f397d2b
Juan Díaz <juan.diaz.12069@gmail.com>
2020-01-29 10:27:26 -0300

    delete post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -91,4 +91,36 @@ router.get('/users/:id', auth, async (req, res) => {
     }
 });
 
+// @route   DELETE api/posts/:id
+// @des     Delete post by id
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        // Get post
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404)
+                .json({ msg: 'Posts not found' });
+        }
+
+        // Check if user is own
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized ' })
+        }
+
+        // Remove
+        await post.remove();
+
+        res.json({ msg: 'Post deleted' });
+    } catch (error) {
+        console.error(error.message);
+        if (error.kind == 'ObjectId') {
+            return res.status(404)
+                .json({ msg: 'Posts not found' });
+        }
+        res.status(500).send('Server error');
+    }
+});
+
 module.exports = router;
