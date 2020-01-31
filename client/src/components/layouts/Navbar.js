c2071b1e33332cfa5e1a8e75140f71c2129c7786
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-01-31 01:22:32 -0300

    start dashboard

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -9,9 +9,15 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     const authLinks = (
         <ul>
             <li>
-                <a href="#!" onClick={logout}>
-                    <i className="fas fa-sign-out-alt"></i>{' '}
-                    <span className="hide-sm">Logout</span>
+                <Link to='/dashboard'>
+                    <i className='fas fa-user'></i>{' '}
+                    <span className='hide-sm'>Dashboard</span>
+                </Link>
+            </li>
+            <li>
+                <a href='#!' onClick={logout}>
+                    <i className='fas fa-sign-out-alt'></i>{' '}
+                    <span className='hide-sm'>Logout</span>
                 </a>
             </li>
         </ul>
@@ -19,16 +25,16 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
     const guestLinks = (
         <ul>
-            <li><Link to="#!">Developers</Link></li>
-            <li><Link to="/register">Register</Link></li>
-            <li><Link to="/login">Login</Link></li>
+            <li><Link to=''>Developers</Link></li>
+            <li><Link to='/register'>Register</Link></li>
+            <li><Link to='/login'>Login</Link></li>
         </ul>
     );
 
     return (
-        <nav className="navbar bg-dark">
+        <nav className='navbar bg-dark'>
             <h1>
-                <Link to="#!"><i className="fas fa-code"></i> DevConnector</Link>
+                <Link to=''><i className='fas fa-code'></i> DevConnector</Link>
             </h1>
             {!loading && (
                 <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
