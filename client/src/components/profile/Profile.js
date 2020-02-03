174b52b5bf64bfb921409d2b5dfad24fa43561a9
Juan Díaz <juan.diaz.12069@gmail.com>
2020-02-03 12:48:10 -0300

    profile, add edit bottom if own profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,5 +1,6 @@
-import React, { Fragment, useEffect } from 'react'
-import PropTypes from 'prop-types'
+import React, { Fragment, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layouts/Spinner';
 import { getProfileById } from '../../actions/profile';
@@ -10,9 +11,21 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
     }, [getProfileById, match.params.id])
 
     return (
-        <div>
-            Profile
-        </div>
+        <Fragment>
+            {profile === null || loading ? (
+                <Spinner />
+            ) : (
+                    <Fragment>
+                        <Link to='/profiles' className='btn btn-light'>Back to Profiles</Link>
+                        {auth.isAuthenticated && auth.loading === false &&
+                            auth.user._id === profile.user._id && (
+                                <Link to='/edit-profile' className='btn btn-dark'>
+                                    Edit Profile
+                            </Link>
+                            )}
+                    </Fragment>
+                )}
+        </Fragment>
     )
 }
 
