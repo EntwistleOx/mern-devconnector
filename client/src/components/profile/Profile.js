ea2db7bbc3536f3403594a66878cccd4018f547e
Juan Díaz <juan.diaz.12069@gmail.com>
2020-02-04 11:29:17 -0300

    get githubrepos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -47,13 +48,16 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                                 {profile.education.length > 0 ? (
                                     <Fragment>
                                         {profile.education.map(education => (
-                                            <ProfileEducation key={education._id} experience={education} />
+                                            <ProfileEducation key={education._id} education={education} />
                                         ))}
                                     </Fragment>
                                 ) : (
                                         <h4>No education credentials</h4>
                                     )}
                             </div>
+                            {profile.githubusername && (
+                                <ProfileGithub username={profile.githubusername} />
+                            )}
                         </div>
                     </Fragment>
                 )}
